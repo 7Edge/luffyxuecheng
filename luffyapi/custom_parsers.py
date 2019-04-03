ba475e823e506114687ded6315bbc0e15f4ba8c9
ZJQi <zhangjiaqi77777@outlook.com>
2019-04-03 18:50:45 +0800

    添加python忽略文件;

---
+++ luffyapi/custom_parsers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=UTF-8
+# author: zhangjiaqi <1399622866@qq.com>
+# File: customparsers
+# Date: 2/26/2019
+from rest_framework import renderers
+
+
+# 继承JSON
+class JSONCustomParser(renderers.JSONRenderer):
+
+
+if __name__ == '__main__':
+    pass
